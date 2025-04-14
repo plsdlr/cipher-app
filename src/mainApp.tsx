67595ad835867fa488774b21bd288cb6a2717aad
p <paulseidler1@gmail.com>
2025-04-14 20:27:07 +0200

    before deterministic rewrite

--- src/mainApp.tsx
+++ src/mainApp.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import WalletPage from './WalletPage.tsx'
-// import CipherWrapper from './canvasWrapper.tsx'
+import CipherWrapper from './canvasWrapper.tsx'
 
 import MintPage from './Mint.tsx'
 
@@ -16,6 +16,107 @@ function MainApp() {
         { id: 'market', label: 'MARKET' }
     ];
 
+    /// delete this later
+
+    const testvalues = {
+        "coordinates": [
+            {
+                "x": 86,
+                "y": 191
+            },
+            {
+                "x": 207,
+                "y": 15
+            },
+            {
+                "x": 157,
+                "y": 52
+            },
+            {
+                "x": 70,
+                "y": 50
+            },
+            {
+                "x": 230,
+                "y": 106
+            },
+            {
+                "x": 163,
+                "y": 192
+            },
+            {
+                "x": 197,
+                "y": 16
+            },
+            {
+                "x": 238,
+                "y": 144
+            },
+            {
+                "x": 243,
+                "y": 35
+            },
+            {
+                "x": 30,
+                "y": 218
+            },
+            {
+                "x": 139,
+                "y": 167
+            },
+            {
+                "x": 211,
+                "y": 36
+            },
+            {
+                "x": 67,
+                "y": 93
+            },
+            {
+                "x": 224,
+                "y": 239
+            },
+            {
+                "x": 147,
+                "y": 41
+            },
+            {
+                "x": 79,
+                "y": 64
+            },
+            {
+                "x": 212,
+                "y": 57
+            },
+            {
+                "x": 54,
+                "y": 156
+            },
+            {
+                "x": 141,
+                "y": 53
+            },
+            {
+                "x": 254,
+                "y": 100
+            }
+        ],
+        "speed": 1,
+        "colorScheme": "red",
+        "walkerTurmites": [
+            "ff0000ff0801000000000200"
+        ],
+        "builderTurmites": [
+            "ff0800ff0201ff0800000001",
+            "ff0801000200000800ff0800",
+            "ff0201000201ff0400000000"
+        ],
+        "chaosNumbers": [
+            2,
+            5,
+            6
+        ]
+    }
 
     // Function to handle menu item selection
     const handleMenuSelect = (menuId) => {
@@ -27,9 +128,15 @@ function MainApp() {
             case 'home':
                 return (
                     <>
-                        <h3>Welcome to Cipher Wallet</h3>
-                        <p>Your secure, decentralized solution for managing digital assets.
-                            Use the menu above to navigate through different sections of the application.</p>
+                    //     <h3>Welcome to Cipher Wallet</h3>
+                    //     <p>Your secure, decentralized solution for managing digital assets.
+                    //         Use the menu above to navigate through different sections of the application.</p>
+                        <CipherWrapper coordinates={testvalues.coordinates}
+                            speed={1} colorScheme='red'
+                            walkerTurmites={testvalues.walkerTurmites}
+                            builderTurmites={testvalues.builderTurmites}
+                            chaosNumbers={testvalues.chaosNumbers} />
+
                     </>
                 );
             case 'mint':
